fe10de4fdd2fece812cabe183bd417a8eece078e
colkadome <josephdunne1993@gmail.com>
2024-02-09 19:54:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+
+/**
+ * Resizes an image.
+ */
+function resizeImage(imageSource, options, callback) {
+
+}
+
+/**
+ * Exports.
+ */
+export default resizeImage;
+export { resizeImage };
